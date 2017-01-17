036082a7e20acbd3f78f09ec2cf39ada0a282ddb
thebarbershopper <cld251@gmail.com>
2017-01-16 22:10:30 -0600

    Use local pykd dlls instead of downloading from a different repo

--- windbg.ps1
+++ windbg.ps1
@@ -1,6 +1,6 @@
 choco install -y windbg
-echo "Installing pykd"
+Write-Host "Installing pykd" -foregroundcolor "Green" -backgroundcolor "Black"
 pip2 install pykd
 pip3 install pykd
-((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-setup/raw/master/pykd-dlls/x86/pykd.dll', 'C:\Program Files\Windows Kits\8.1\Debuggers\x86\winext\pykd.dll'))
-((new-object system.net.webclient).downloadfile('https://github.com/ctfhacker/windows-setup/raw/master/pykd-dlls/x64/pykd.dll', 'C:\Program Files\Windows Kits\8.1\Debuggers\x64\winext\pykd.dll'))
+copy pykd-dlls/x86/pykd.dll "C:\Program Files\Windows Kits\8.1\Debuggers\x86\winext\pykd.dll"
+copy pykd-dlls/x64/pykd.dll "C:\Program Files\Windows Kits\8.1\Debuggers\x64\winext\pykd.dll"
