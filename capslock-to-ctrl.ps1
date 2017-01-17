9dba7f79c5f9e4aa6edbf0ba8137f71704361195
thebarbershopper <cld251@gmail.com>
2017-01-16 22:06:04 -0600

    foreground -> foregroundcolor

--- capslock-to-ctrl.ps1
+++ capslock-to-ctrl.ps1
@@ -1,2 +1,2 @@
-echo "Remapping CapsLock to Ctrl"
+Write-Host "Remapping CapsLock to Ctrl" -foregroundcolor "Green" -backgroundcolor "Black"
 reg add "HKLM\System\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000000020000001d003a0000000000
--- cmder.ps1
+++ cmder.ps1
@@ -1,6 +1,6 @@
-Write-Host "Installing Cmder" -foreground "Green" -background "Black"
+Write-Host "Installing Cmder" -foregroundcolor "Green" -backgroundcolor "Black"
 choco install -y cmder
-Write-Host "Importing Cmder settings" -foreground "Green" -background "Black"
+Write-Host "Importing Cmder settings" -foregroundcolor "Green" -backgroundcolor "Black"
 reg import Cmder.reg
 
 git clone https://github.com/ctfhacker/cmder-dotfiles dotfiles
