3492ea0bc55eecaecdb7293345e2f9f0afd234d7
thebarbershopper <cld251@gmail.com>
2017-01-17 15:48:08 -0600

    Utilize unzip for installing dynamorio

--- dynamorio.ps1
+++ dynamorio.ps1
@@ -1,5 +1,9 @@
 pushd
 ((new-object system.net.webclient).downloadfile('https://github.com/DynamoRIO/dynamorio/releases/download/release_6_2_0/DynamoRIO-Windows-6.2.0-2.zip', 'C:\tools\dynamorio.zip'))
+((new-object system.net.webclient).downloadfile('https://github.com/DynamoRIO/drmemory/releases/download/release_1.11.0/DrMemory-Windows-1.11.0-2.zip', 'C:\tools\drmemory.zip'))
 cd C:\tools
-[System.IO.Compression.ZipFile]::ExtractToDirectory('dynamorio.zip', 'dynamorio')
+unzip dynamorio.zip
+rm dynamorio.zip
+unzip drmemory.zip
+rm drmemory.zip
 popd
