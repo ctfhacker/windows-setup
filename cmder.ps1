d0dc37ec7bf6f1e2a8854c07010958fd7388c691
thebarbershopper <cld251@gmail.com>
2017-01-16 23:09:05 -0600

    cmder wasn't installing dotfiles to the correct location

--- cmder.ps1
+++ cmder.ps1
@@ -3,7 +3,9 @@ choco install -y cmder
 Write-Host "Importing Cmder settings" -foregroundcolor "Green" -backgroundcolor "Black"
 reg import Cmder.reg
 
+pushd
+cd $HOME
 git clone https://github.com/ctfhacker/cmder-dotfiles dotfiles
 cd dotfiles
 .\install.bat
-cd ..
+popd
