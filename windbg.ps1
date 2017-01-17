31f2d7e0351f6a08de42180f5b6ab9a575c54475
thebarbershopper <cld251@gmail.com>
2017-01-16 22:55:01 -0600

    Add full path to pip's

--- windbg.ps1
+++ windbg.ps1
@@ -1,6 +1,6 @@
 choco install -y windbg
 Write-Host "Installing pykd" -foregroundcolor "Green" -backgroundcolor "Black"
-pip2 install pykd
-pip3 install pykd
+C:\Python2*\Scripts\pip2.exe install pykd
+C:\Python3*\Scripts\pip3.exe install pykd
 copy pykd-dlls/x86/pykd.dll "C:\Program Files\Windows Kits\8.1\Debuggers\x86\winext\pykd.dll"
 copy pykd-dlls/x64/pykd.dll "C:\Program Files\Windows Kits\8.1\Debuggers\x64\winext\pykd.dll"
