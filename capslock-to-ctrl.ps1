e9197f98c46ed90b67000be0fe15fe8a70aea7e4
thebarbershopper <cld251@gmail.com>
2017-01-18 13:38:28 -0600

    Force overwriting codemap for capslock -> ctrl

--- capslock-to-ctrl.ps1
+++ capslock-to-ctrl.ps1
@@ -1,2 +1,2 @@
 Write-Host "Remapping CapsLock to Ctrl" -foregroundcolor "Green" -backgroundcolor "Black"
-reg add "HKLM\System\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000000020000001d003a0000000000
+reg add /f "HKLM\System\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /t REG_BINARY /d 0000000000000000020000001d003a0000000000
