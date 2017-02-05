1a73a508ea13046f0b71a0943e7460774262a823
thebarbershopper <cld251@gmail.com>
2017-02-04 21:19:57 -0600

    32/64 bit windbg check

--- windbg.ps1
+++ windbg.ps1
@@ -7,15 +7,15 @@ Write-Host "Installing pykd" -foregroundcolor "Green" -backgroundcolor "Black"
 C:\Python2*\Scripts\pip2.exe install pykd
 C:\Python3*\Scripts\pip3.exe install pykd
 
-$p = "Program Files"
+$p = "Program Files (x86)"
 if ([System.IntPtr]::Size -eq 4){
-    $p = "Program Files (x86)"
+    $p = "Program Files"
 }
 
-copy pykd-dlls/x86/pykd.dll "C:\" + $p + "\Windows Kits\8.0\Debuggers\x86\winext\pykd.dll"
-copy pykd-dlls/x86/pykd.dll "C:\" + $p + "\Windows Kits\8.1\Debuggers\x86\winext\pykd.dll"
-copy pykd-dlls/x86/pykd.dll "C:\" + $p + "\Windows Kits\10\Debuggers\x86\winext\pykd.dll"
-copy pykd-dlls/x64/pykd.dll "C:\" + $p + "\Windows Kits\8.0\Debuggers\x64\winext\pykd.dll"
+copy pykd-dlls/x86/pykd.dll "C:\$($p)\Windows Kits\8.0\Debuggers\x86\winext\pykd.dll"
+copy pykd-dlls/x86/pykd.dll "C:\$($p)\Windows Kits\8.1\Debuggers\x86\winext\pykd.dll"
+copy pykd-dlls/x86/pykd.dll "C:\$($p)\Windows Kits\10\Debuggers\x86\winext\pykd.dll"
+copy pykd-dlls/x64/pykd.dll "C:\$($p)\Windows Kits\8.0\Debuggers\x64\winext\pykd.dll"
 copy pykd-dlls/x64/pykd.dll "C:\" + $p+ "\Windows Kits\8.1\Debuggers\x64\winext\pykd.dll"
 copy pykd-dlls/x64/pykd.dll "C:\" + $p+ "\Windows Kits\10\Debuggers\x64\winext\pykd.dll"
 
