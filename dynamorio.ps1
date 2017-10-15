bec17eb1a8cc31695d6ff396d9669ee3e6ec9f59
thebarbershopper <cld251@gmail.com>
2017-10-15 11:28:35 -0500

    Update versions on DynamoRIO

--- dynamorio.ps1
+++ dynamorio.ps1
@@ -1,5 +1,5 @@
 pushd
-((new-object system.net.webclient).downloadfile('https://github.com/DynamoRIO/dynamorio/releases/download/release_6_2_0/DynamoRIO-Windows-6.2.0-2.zip', 'C:\tools\dynamorio.zip'))
+((new-object system.net.webclient).downloadfile('https://github.com/DynamoRIO/dynamorio/releases/download/release_7_0_0_rc1/DynamoRIO-Windows-7.0.0-RC1.zip', 'C:\tools\dynamorio.zip'))
 ((new-object system.net.webclient).downloadfile('https://github.com/DynamoRIO/drmemory/releases/download/release_1.11.0/DrMemory-Windows-1.11.0-2.zip', 'C:\tools\drmemory.zip'))
 cd C:\tools
 unzip dynamorio.zip
