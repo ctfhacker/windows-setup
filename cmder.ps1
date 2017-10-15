e283ada16aa6e83b02b1cc53d81cac6a12e241f0
thebarbershopper <cld251@gmail.com>
2017-10-15 11:30:30 -0500

    Change cmder dotfiles dir

--- cmder.ps1
+++ cmder.ps1
@@ -12,7 +12,7 @@ refreshenv
 Write-Host "Installing Cmder dotfiles" -foregroundcolor "Green" -backgroundcolor "Black"
 pushd
 cd C:\home\$Env:USERNAME
-& "C:\Program Files\Git\bin\git.exe" clone https://github.com/ctfhacker/cmder-dotfiles dotfiles
-cd dotfiles
+& "C:\Program Files\Git\bin\git.exe" clone https://github.com/ctfhacker/cmder-dotfiles
+cd cmder-dotfiles
 .\install.bat
 popd
